f6994e02d1b0033dd4a262a7ee66c1699c20c1d9
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-10-07 15:10:46 -0700

    Add facebook client

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -10,20 +10,42 @@ import (
 type CoreHandler struct{}
 
 func (api *CoreHandler) GetPosts(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
 
-	// Testing begin
-	posts := make([]models.Post, 0)
-	hnResp, err := http.Get("http://hacker-news:4000/v1/posts?count=20")
+	// TODO: Abstract this, make it concurrent
+	hnPosts := make([]models.Post, 0)
+	hnResp, err := http.Get("http://hacker-news-client:4000/v1/posts?count=20")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = json.NewDecoder(hnResp.Body).Decode(&posts)
+	err = json.NewDecoder(hnResp.Body).Decode(&hnPosts)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// Testing end
+
+	var fbId string
+	var fbToken string
+	if v, ok := query["fb_id"]; ok && len(v) != 0 {
+		fbId = v[0]
+	}
+	if v, ok := query["fb_token"]; ok && len(v) != 0 {
+		fbToken = v[0]
+	}
+	fbPosts := make([]models.Post, 0)
+	fbResp, err := http.Get("http://facebook-client:5000/v1/posts?fb_id="+fbId+"&fb_token="+fbToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewDecoder(fbResp.Body).Decode(&fbPosts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	posts := append(hnPosts, fbPosts...)
 
 	res, err := json.Marshal(posts)
 	if err == nil {
