2cc0608c8705da5e989c5bf6678db60eb7ed827b
Jonathon Gore <jackgore@hotmail.ca>
2017-12-05 22:19:36 -0800

    Updated url

--- scripts/runProdDaemon.sh
+++ scripts/runProdDaemon.sh
@@ -1,1 +1,1 @@
-ssh -i ~/keys/iced-mocha-pair.pem -NL localhost:2374:/var/run/docker.sock docker@ec2-52-15-230-24.us-east-2.compute.amazonaws.com &
+ssh -i ~/keys/iced-mocha-pair.pem -NL localhost:2374:/var/run/docker.sock docker@ec2-18-220-185-156.us-east-2.compute.amazonaws.com &
